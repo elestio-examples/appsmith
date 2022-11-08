efa2f46cc93e814de7d75acbcb7fbb9a6b8e7f22
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-11-08 19:46:49 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -20,7 +20,13 @@ target=$(docker-compose port appsmith 80)
 
 #   sleep 60s;
 
-
+curl http://${target}/api/v1/users/me \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'cache-control: no-cache' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
+  --compressed
 
 curl http://${target}/api/v1/users/super \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
